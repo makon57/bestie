5e41329b328f668e087a2daa43771646d3f51195
Manna Kong <m.kong.57@gmail.com>
2021-08-24 21:38:10 -0500

    css navbar

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -6,33 +6,48 @@ import './NavBar.css'
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className="main-nav">
+      <div className="menu-wrap">
+        <div className="menu-left-container">
+          <ul>
+            <li>
+              <NavLink to='/' exact={true} activeClassName='active'>
+                <img className='bestie-logo2'src='https://i.imgur.com/sKEQCij.png' alt='logo-2'></img>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/login' exact={true} activeClassName='active'>
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/sign-up' exact={true} activeClassName='active'>
+                Sign Up
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <h1 id='logo'>
           <NavLink to='/' exact={true} activeClassName='active'>
-            Home
+            <img className='bestie-logo' src='https://i.imgur.com/ZX8v9Tq.png' alt='bestie-logo'></img>
           </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-      <img src='' alt='bestie-logo'></img>
+        </h1>
+        <div>
+          <ul>
+            <li>
+              <NavLink to='/users' exact={true} activeClassName='active'>
+                Users
+              </NavLink>
+            </li>
+            <li>
+              <p>Profile</p>
+            </li>
+            <li>
+              <LogoutButton />
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
