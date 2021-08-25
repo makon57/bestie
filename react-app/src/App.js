e9502e0d2d7a1989b8167ab346f0000f746eb918
Manna Kong <m.kong.57@gmail.com>
2021-08-24 22:44:32 -0500

    footer all done

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar/NavBar'
+import Footer from './components/Footer';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
@@ -44,6 +45,7 @@ function App() {
           <h1>My Home Page</h1>
         </ProtectedRoute>
       </Switch>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ react-app/src/components/Footer/index.js
@@ -0,0 +1,33 @@
+import '../Footer/Footer.css'
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <div className='main-footer'>
+      <div className="footer-wrap">
+        <div className="footer-left-container">
+          <div>
+            <Link to='https://github.com/makon57' exact={true} activeClassName='active'>
+                <p>GITHUB</p>
+            </Link>
+          </div>
+        </div>
+        <div className='logo-angelist'>
+          <Link to='https://angel.co/u/manna-kong' exact={true} activeClassName='active'>
+            <img className='bestie-logo-footer' src='https://i.imgur.com/ZX8v9Tq.png' alt='bestie-logo'></img>
+            <p className='footer-center-container'>ANGELIST</p>
+          </Link>
+        </div>
+        <div className="footer-right-container">
+          <div>
+            <Link to='https://www.linkedin.com/in/manna-kong/' exact={true} activeClassName='active'>
+                <p>LINKEDIN</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+  export default Footer;
