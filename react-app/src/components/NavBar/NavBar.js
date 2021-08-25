87d45b01090bcf96d743f5955f7ede92a00d00eb
Manna Kong <m.kong.57@gmail.com>
2021-08-24 23:18:32 -0500

    working on nav

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -9,43 +9,39 @@ const NavBar = () => {
     <nav className="main-nav">
       <div className="menu-wrap">
         <div className="menu-left-container">
-          <ul>
-            <li>
-              <NavLink to='/' exact={true} activeClassName='active'>
-                <img className='bestie-logo2'src='https://i.imgur.com/sKEQCij.png' alt='logo-2'></img>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to='/login' exact={true} activeClassName='active'>
-                Login
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to='/sign-up' exact={true} activeClassName='active'>
-                Sign Up
-              </NavLink>
-            </li>
-          </ul>
+          <div className='logo'>
+            <NavLink to='/' exact={true} activeClassName='active'>
+              <img className='bestie-logo2'src='https://i.imgur.com/sKEQCij.png' alt='logo-2'></img>
+            </NavLink>
+          </div>
+          <div className='adopt'>
+            <NavLink to='/login' exact={true} activeClassName='active'>
+              Login
+            </NavLink>
+          </div>
+          <div className='foster'>
+            <NavLink to='/sign-up' exact={true} activeClassName='active'>
+              Sign Up
+            </NavLink>
+          </div>
         </div>
-        <h1 id='logo'>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            <img className='bestie-logo' src='https://i.imgur.com/ZX8v9Tq.png' alt='bestie-logo'></img>
-          </NavLink>
-        </h1>
-        <div>
-          <ul>
-            <li>
-              <NavLink to='/users' exact={true} activeClassName='active'>
-                Users
-              </NavLink>
-            </li>
-            <li>
-              <p>Profile</p>
-            </li>
-            <li>
-              <LogoutButton />
-            </li>
-          </ul>
+        <NavLink to='/' exact={true} activeClassName='active'>
+          <img className='bestie-logo' src='https://i.imgur.com/ZX8v9Tq.png' alt='bestie-logo'></img>
+        </NavLink>
+        <div className='menu-right-container'>
+          <div className='donate'>
+            <NavLink to='/users' exact={true} activeClassName='active'>
+              Users
+            </NavLink>
+          </div>
+          <div className='about'>
+            <NavLink to='/users/:id' exact={true} activeClassName='active'>
+              Prodile
+            </NavLink>
+          </div>
+          <div className='auth'>
+            <LogoutButton />
+          </div>
         </div>
       </div>
     </nav>
