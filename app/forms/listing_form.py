0b10f94f67a70e1dc809d2783ec5631d05b91f96
Manna Kong <m.kong.57@gmail.com>
2021-09-03 00:07:04 -0500

    changed validation for age

--- app/forms/listing_form.py
+++ app/forms/listing_form.py
@@ -1,15 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, IntegerField, TextField
 # from wtforms import validators
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
 # from app.models import Listing
 
+def valid_age(form, field):
+    age = field.data
+    if age < 0:
+        raise ValidationError("You must be 18 and over to adopt a bestie.")
 
 class ListingCreateForm(FlaskForm):
     user_id = IntegerField()
     name = StringField('Name', validators=[DataRequired(), Length(min=0, max=50, message="Name must be less than 50 characters long.")])
     gender = StringField('Gender', validators=[DataRequired()])
-    age = IntegerField('Age', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired(), valid_age])
     pet_type = StringField('Pet Type', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired(), Length(min=0, max=2000)])
     image_url = TextField('Image')
