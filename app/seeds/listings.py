e8b8a26c9418798244c86864f34816f50c4ac270
Manna Kong <m.kong.57@gmail.com>
2023-11-16 12:52:41 -0600

    seeding
    Signed-off-by: Manna Kong <m.kong.57@gmail.com>

--- app/seeds/listings.py
+++ app/seeds/listings.py
@@ -36,43 +36,43 @@ def seed_listings():
     pet2 = Listing(
         user_id=2, name="Bubble", gender="Male", age=8, pet_type="Dog",
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        image="https://s3.amazonaws.com/filestore.rescuegroups.org/3699/pictures/animals/16378/16378454/79833680_500x500.jpg",
+        image="https://i.imgur.com/SPUSU8N.jpg",
         created_at=datetime.now(), updated_at=datetime.now()
     )
     pet3 = Listing(
         user_id=4, name="Rocker", gender="Female", age=1, pet_type="Guinea Pig",
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        image="https://assets.animalhumanesociety.org/animalhumanesociety.org/files/styles/animal_450x330/public/adoption/images/large/2021/08/15/46766912-1668-4635-b558-47c5b95c775f.jpg?h=8a14a818&itok=quvM1otg",
+        image="https://i.imgur.com/CUbPRnN.jpg",
         created_at=datetime.now(), updated_at=datetime.now()
     )
     pet4 = Listing(
         user_id=4, name="Scarlet", gender="Female", age=2, pet_type="Dog",
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        image="https://s3.amazonaws.com/filestore.rescuegroups.org/3699/pictures/animals/16817/16817818/78944359_500x500.jpg",
+        image="https://i.imgur.com/4Ppbpzt.jpg",
         created_at=datetime.now(), updated_at=datetime.now()
     )
     pet5 = Listing(
         user_id=5, name="Dragon", gender="Male", age=9, pet_type="Dog",
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        image="https://s3.amazonaws.com/filestore.rescuegroups.org/3699/pictures/animals/16613/16613658/80250886_363x484.jpg",
+        image="https://i.imgur.com/UNsxdjz.jpg",
         created_at=datetime.now(), updated_at=datetime.now()
     )
     pet6 = Listing(
         user_id=5, name="Stripes", gender="Male", age=5, pet_type="Cat",
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        image="https://s3.amazonaws.com/filestore.rescuegroups.org/3699/pictures/animals/16984/16984997/80560141_500x500.jpg",
+        image="https://i.imgur.com/DIO9mBK.jpg",
         created_at=datetime.now(), updated_at=datetime.now()
     )
     pet7 = Listing(
         user_id=2, name="Tofu", gender="Male", age=4, pet_type="Cat",
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        image="https://s3.amazonaws.com/filestore.rescuegroups.org/3699/pictures/animals/16870/16870388/80253838_500x500.jpg",
+        image="https://i.imgur.com/r3y6dq9.jpg",
         created_at=datetime.now(), updated_at=datetime.now()
     )
     pet8 = Listing(
         user_id=4, name="Fiona", gender="Female", age=2, pet_type="Dog",
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        image="https://s3.amazonaws.com/filestore.rescuegroups.org/3699/pictures/animals/17125/17125776/81266349_500x503.jpg",
+        image="https://i.imgur.com/oEHar9a.jpg",
         created_at=datetime.now(), updated_at=datetime.now()
     )
 
